1b9416e4515a71db9da8df434c21a041b205a8ce
drdeckto <fynjy128@gmail.com>
2025-03-01 19:24:24 +0300

    init

---
+++ src/main/java/by/drdeck/usersub/UserSubApplication.java
@@ -0,0 +1,13 @@
+package by.drdeck.usersub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/drdeck/usersub/UserSubApplicationTests.java
@@ -0,0 +1,13 @@
+package by.drdeck.usersub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
